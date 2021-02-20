e37e00ccc7255dff69640d35381be14a3afa4f77
Tarun Sikhwal <69427847+TarunSikhwal@users.noreply.github.com>
2021-02-20 13:44:39 +0530

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6030bca1465b2a001e0e977c",
+				"6030bca1465b2a001e0e977e"
+			]
+		},
+		"6030bca1465b2a001e0e977c": {
+			"id": "6030bca1465b2a001e0e977c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6030bca1465b2a001e0e977e": {
+			"id": "6030bca1465b2a001e0e977e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
